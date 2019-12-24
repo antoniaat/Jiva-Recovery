6b8d9060405d49424e684beef03b8b1197f4919a
Antonia Atanasova <antoniyaata@gmail.com>
2019-12-24 15:29:47 +0200

    Deploy the website ot netifly

---
+++ jivatrade/build/precache-manifest.51ed20b3c10560fd7972acf5dd25ce7e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb41d7c7e39e4cf5ffdc8df389836db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c5352802a4b1d45ac9",
+    "url": "/static/css/main.c69aa02c.chunk.css"
+  },
+  {
+    "revision": "6f7fee8f259cae7a874f",
+    "url": "/static/js/2.2abde61e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.2abde61e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17c5352802a4b1d45ac9",
+    "url": "/static/js/main.aea82af7.chunk.js"
+  },
+  {
+    "revision": "fcabc0eb07879effa461",
+    "url": "/static/js/runtime-main.2e616333.js"
+  },
+  {
+    "revision": "247fe83af1f2e904ffcafd21d79ee495",
+    "url": "/static/media/1.247fe83a.png"
+  },
+  {
+    "revision": "3e8fa2a72e55a1bc53942a8cadcfb862",
+    "url": "/static/media/1.3e8fa2a7.png"
+  },
+  {
+    "revision": "dd4d5299c9d9a717db2bcdd289a35ad5",
+    "url": "/static/media/10.dd4d5299.png"
+  },
+  {
+    "revision": "94dcc69b4879b04f3875b2a17ade9960",
+    "url": "/static/media/2.94dcc69b.png"
+  },
+  {
+    "revision": "b913944da22364d89d2a5b5821edf709",
+    "url": "/static/media/2.b913944d.png"
+  },
+  {
+    "revision": "5df65c6c51bafec68cd05e9da367dd3a",
+    "url": "/static/media/3.5df65c6c.png"
+  },
+  {
+    "revision": "b188f8ba32dc147fc877ddf961d67f62",
+    "url": "/static/media/4.b188f8ba.png"
+  },
+  {
+    "revision": "81f073f56af2f861aa149b4b5e745ba2",
+    "url": "/static/media/5.81f073f5.png"
+  },
+  {
+    "revision": "0d17e38b01ec0c00f062165767cb4dc3",
+    "url": "/static/media/6.0d17e38b.png"
+  },
+  {
+    "revision": "eebaa3db756262bda1c080786e401946",
+    "url": "/static/media/7.eebaa3db.png"
+  },
+  {
+    "revision": "501c9b5b3ee0db292338b2cc18458851",
+    "url": "/static/media/8.501c9b5b.png"
+  },
+  {
+    "revision": "a4d17c69cc2ebead50b13ff604d6174a",
+    "url": "/static/media/9.a4d17c69.png"
+  },
+  {
+    "revision": "20a6561bfaa50e8f8e5f16a9312b7762",
+    "url": "/static/media/logo.20a6561b.png"
+  }
+]);
